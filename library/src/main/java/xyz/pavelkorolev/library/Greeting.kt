baf40a2638a7d72489a6de334ba8319d47dfc533
Pavel Korolev <pavelkorolevxyz@gmail.com>
2022-05-16 13:43:20 +0400

    Added sample warnings

--- library/src/main/java/xyz/pavelkorolev/library/Greeting.kt
+++ library/src/main/java/xyz/pavelkorolev/library/Greeting.kt
@@ -3,7 +3,8 @@ package xyz.pavelkorolev.library
 import android.content.Context
 import android.widget.Toast
 
-class Greeting {
+class Greeting
+{
 
     fun sayHello(context: Context) {
         Toast.makeText(context, "Hello, world!", Toast.LENGTH_SHORT).show()
